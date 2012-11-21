a3b0e64f40cd80d63347b0f2518fc1d5e6a8493b
Carol Nichols <cnichols@thinkthroughmath.com>
2012-11-21 12:35:34 -0500

    Create a new rails 3.2 sample app for testing

---
+++ sample_app_3_2/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp32::Application.config.secret_token = 'e4e560ffa99f78e692e5f3895ca6a13496d1871aeb04628128e3f102cb2f89fea456e04c944a558f9a269cdf2a9b4ebc6ce2cc99d18d4d949240c1c275de7808'
