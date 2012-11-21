406f10bcc46a55064c0ff70b20ce79906052a832
Carol Nichols <cnichols@thinkthroughmath.com>
2012-11-21 13:47:27 -0500

    Trying out an untested fix for an issue i think we're seeing as documented here: https://github.com/tchandy/octopus/issues/36

--- lib/octopus/model.rb
+++ lib/octopus/model.rb
@@ -97,7 +97,7 @@ module Octopus::Model
     end
 
     def should_set_current_shard?
-      self.respond_to?(:current_shard) && !self.current_shard.nil?
+      self.respond_to?(:current_shard) && !self.current_shard.nil? && !self.class.connection_proxy.block
     end
 
     def reload_connection_safe(&block)
